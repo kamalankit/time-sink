f5dc2c4b5b1c675764bbe5983f7a4a99219c9189
kamalankit <riasdragon999@gmail.com>
2025-07-13 22:32:59 +0530

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,6 @@
-// metro.config.js - Alternative simpler configuration
+// metro.config.js - Minimal configuration
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-// This helps resolve the getDevServer error
-config.resolver.platforms = ['ios', 'android', 'web'];
-
-// Remove problematic SVG from asset extensions if present
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-
-// Add SVG to source extensions for proper handling
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
-
-// Reset transformer to default
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
-};
 
 module.exports = config;
